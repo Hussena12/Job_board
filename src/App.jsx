1987cfcbfa866cf5cd7cd1e3633bdfddf8cad252
Hussen <ha0959943@gmail.com>
2025-08-14 21:13:50 +0300

    feat: create layout suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,123 @@
 import "./App.css";
-import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import {
-  EditJob,
-  Home,
-  JobDetails,
-  Jobs,
-  Login,
-  PostJob,
-  Register,
-  AboutUs,
-  ContactUs,
-} from "./pages";
-
-import { Dashboard, MyPostedJob } from "./Dashboard";
-
-import { Navbar } from "./layout";
-import { useStateContext } from "./contexts/useStateContext";
-import { PrivateRoute } from "./components";
+import { lazy, Suspense } from "react";
+import { Spinner } from "./components";
+import MainLayout from "./layout/MainLayout";
 import { JobProvider } from "./contexts/JobContext";
 import { SavedjobsProvider } from "./contexts/useSavedJobs";
+import { PrivateRoute } from "./components";
+
+import { Home } from "./pages";
+// lazy loaded pages
+import { Jobs } from "../src/utils/lazyPages";
+import { JobDetails } from "../src/utils/lazyPages";
+import { Login } from "../src/utils/lazyPages";
+import { PostJob } from "../src/utils/lazyPages";
+import { Register } from "../src/utils/lazyPages";
+import { AboutUs } from "../src/utils/lazyPages";
+import { ContactUs } from "../src/utils/lazyPages";
+import { EditJob } from "../src/utils/lazyPages";
+
+const Dashboard = lazy(() => import("./Dashboard/userId/DashNavbar"));
+const MyPostedJob = lazy(() => import("./Dashboard/userId/MyPostedJob"));
 
 function App() {
   const location = useLocation();
   const hideNavbarOn = ["/Login", "/Register", "/PostJob", "/Dashboard"];
   const shouldHideNavbar = hideNavbarOn.includes(location.pathname);
 
-  const { currentMode } = useStateContext();
-
   return (
-    <div className={currentMode === "Dark" ? "dark" : ""}>
-      <div className="bg-[#f7f7f7] dark:bg-[#121212] min-h-screen">
-        {!shouldHideNavbar && <Navbar />}
-
-        <div className={!shouldHideNavbar ? "" : ""}>
-          <Routes>
-            {/* pages */}
-            <Route path="/" element={<Home />} />
-            <Route path="/Jobs" element={<Jobs />} />
-            <Route
-              path="/PostJob"
-              element={
-                <PrivateRoute>
-                  <PostJob />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/AboutUs" element={<AboutUs />} />
-            <Route path="/ContactUs" element={<ContactUs />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-
-            {/* dashboard */}
-            <Route
-              path="/Dashboard"
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/MyPostedJob" element={<MyPostedJob />} />
-
-            <Route path="/JobDetails" element={<JobDetails />} />
-            <Route path="/EditJob" element={<EditJob />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <MainLayout shouldHideNavbar={shouldHideNavbar}>
+      <Routes>
+        {/* pages */}
+        <Route path="/" element={<Home />} />{" "}
+        <Route
+          path="/jobs"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Jobs />
+            </Suspense>
+          }
+        />{" "}
+        <Route
+          path="/postJob"
+          element={
+            <PrivateRoute>
+              <Suspense fallback={<Spinner />}>
+                <PostJob />
+              </Suspense>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/AboutUs"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <AboutUs />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/ContactUs"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <ContactUs />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Login"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Register"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Register />
+            </Suspense>
+          }
+        />
+        {/* dashboard */}
+        <Route
+          path="/Dashboard"
+          element={
+            <PrivateRoute>
+              <Suspense fallback={<Spinner />}>
+                <Dashboard />
+              </Suspense>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/MyPostedJob"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <MyPostedJob />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/JobDetails"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <JobDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EditJob"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <EditJob />
+            </Suspense>
+          }
+        />{" "}
+      </Routes>
+    </MainLayout>
   );
 }
 
